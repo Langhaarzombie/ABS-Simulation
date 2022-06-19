e52cf791e22428604669bf099e0c18716bb74417
Langhaarzombie <mixmax199989@gmail.com>
2022-06-19 19:11:14 +0200

    Corrected ecut for potential energy calculation

--- src/simulation.py
+++ src/simulation.py
@@ -32,7 +32,7 @@ def calculate_forces(positions, boundary, sigma, dt):
 
     cut_off = (2**(1/6)*sigma)**2 # Cutoff distance for potential
     s6 = sigma**6
-    ecut = 2*s6*(s6**2/cut_off**6 - 1/cut_off**3)
+    ecut = 2*(s6**2/cut_off**6 - s6/cut_off**3)
 
     for i in np.arange(len(positions)):
         # Loop over unique pairs
